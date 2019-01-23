c0b4dfda0c612a12110786fff08400c6fa045c09
Sopel97 <ts.tomeksopel@gmail.com>
2019-01-23 20:44:27 +0100

    sfml window for preview

--- ray/ray.cpp
+++ ray/ray.cpp
@@ -1,4 +1,16 @@
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
 int main() 
 {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "ray");
 
+    for (;;)
+    {
+        sf::Event e;
+        while (window.pollEvent(e));
+
+        window.clear();
+        window.display();
+    }
 }
