fe533f1c5e3b5b402c8e96921476170d8b34f9bd
Sopel97 <ts.tomeksopel@gmail.com>
2019-01-23 23:35:51 +0100

    basic vector functions

--- ray/src/Vec3.h
+++ ray/src/Vec3.h
@@ -150,6 +150,16 @@ namespace ray
     }
 
     template <typename T>
+    constexpr Vec3<T> operator*(const T& lhs, const Vec3<T>& rhs)
+    {
+        return Vec3<T>(
+            lhs * rhs.x,
+            lhs * rhs.y,
+            lhs * rhs.z
+            );
+    }
+
+    template <typename T>
     constexpr Vec3<T> operator/(const Vec3<T>& lhs, const T& rhs)
     {
         return Vec3<T>(
@@ -176,6 +186,37 @@ namespace ray
             lhs.z*rhs.x - lhs.x*rhs.z, 
             lhs.x*rhs.y - lhs.y*rhs.x
         );
+    }
+
+    template <typename T>
+    constexpr T mixed(const Vec3<T>& a, const Vec3<T>& b, const Vec3<T>& c)
+    {
+        return dot(a, cross(b, c));
+    }
+
+    // projection of v on n
+    template <typename T>
+    constexpr T projection(const Vec3<T>& v, const Vec3<T>& n)
+    {
+        return n * (dot(v, n) / dot(n, n));
+    }
+
+    // reflection of v from a surface with normal n (like light reflection)
+    template <typename T>
+    constexpr Vec3<T> reflection(const Vec3<T>& v, const Vec3<T>& n)
+    {
+        return v - static_cast<T>(2) * projection(v, n);
+    }
+
+    // refraction of v through a surface with normal n with refractive index ratio of r
+    template <typename T>
+    constexpr Vec3<T> refraction(const Vec3<T>& v, const Vec3<T>& n, const T& r)
+    {
+        using std::sqrt;
+
+        const T c = dot(v, n);
+        const T s = sqrt(static_cast<T>(1) - r*r*(static_cast<T>(1) - c*c));
+        return r*v + (r*c - s)*n;
     }
 
     template <typename T>
